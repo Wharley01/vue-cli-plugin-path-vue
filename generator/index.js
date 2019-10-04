9b215e0fe4a2007554bee792251cd9319591cb58
Wharley01 <hackerslord96@gmail.com>
2019-10-04 10:58:59 +0100

    Added post installation log

--- generator/index.js
+++ generator/index.js
@@ -7,4 +7,15 @@ module.exports = api => {
     });
 
     api.render("../template");
+
+    api.exitLog("Vue PathPHP plugin successfully installed \n\n","done");
+
+    api.exitLog("To try the server side rendering, run \n","info");
+    api.exitLog("yarn build \n","log");
+
+    api.exitLog("./path start server \n\n","log");
+
+    api.exitLog("To run from client side, run \n","info");
+    api.exitLog("yarn serve\n","log");
+
 };
